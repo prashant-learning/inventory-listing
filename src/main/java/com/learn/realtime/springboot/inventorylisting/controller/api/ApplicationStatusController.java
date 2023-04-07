98a65cfcf616025dd582becdd61889719e3ca537
prashant-learning <prashantkumar02999@gmail.com>
2023-04-07 18:26:08 +0530

    commit 9

--- src/main/java/com/learn/realtime/springboot/inventorylisting/controller/api/ApplicationStatusController.java
+++ src/main/java/com/learn/realtime/springboot/inventorylisting/controller/api/ApplicationStatusController.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApplicationStatusController {
 
+    private static Logger logger = LoggerFactory.getLogger(ApplicationStatusController.class);
+
     @Tag(name = "Application Status")
     @Operation(summary = "Application status", description = "Application is running or shutdown")
     @ApiResponses(
@@ -28,6 +32,11 @@ public class ApplicationStatusController {
 
     @GetMapping("/status")
     public ResponseEntity<String> getApplicationStatus(){
+
+        for (int i =0 ; i < 1000; i++){
+            logger.trace("Application is shutdown");
+            logger.error("Application is error");
+        }
 
         return ResponseEntity.ok("Application is running");
     }
